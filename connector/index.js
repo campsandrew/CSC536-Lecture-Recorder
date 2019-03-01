6910f571b1b0630cc263575d04af3a15c3eae812
Andrew Camps <andrewcamps@dhcp-10-142-132-8.uawifi.arizona.edu>
2019-03-01 03:04:58 -0700

    created host connector server to recover dynamic ip on frontend

---
+++ connector/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config_file = require("./config.json");
+
+const mongoose_options = { useNewUrlParser: true, useCreateIndex: true };
+const env = process.env.NODE_ENV || "development";
+const config = config_file[env];
+const mainDB = config.database;
+const app = express();
+
+// Host Schema
+const { Schema, model } = mongoose;
+const hostSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  address: { type: String, required: true }
+});
+const Host = model("Host", hostSchema);
+
+// Connect to mongoDB
+mongoose.connect(mainDB, mongoose_options).catch(err => console.log(err));
+
+// Routes defined
+app.use("/connector", connectorRoute);
+app.use(errorRoute);
+
+// Start server
+app.listen(config.node_port, function() {
+  console.log("Listening on Port: " + config.node_port);
+});
+
+/**
+ *
+ */
+function connectorRoute(req, res) {
+  let payload = {
+    success: true
+  };
+
+  Host.findOne({ name: config.server_name })
+    .then(function(doc) {
+      if (!doc) {
+        payload.success = false;
+        message: "no server address found in database";
+      }
+
+      payload.address = doc.address;
+      res.json(payload);
+    })
+    .catch(function(err) {
+      payload.success = false;
+      message: "database error";
+      res.json(payload);
+    });
+}
+
+/**
+ *
+ */
+function errorRoute(req, res) {
+  let payload = {
+    success: false,
+    message: "404 not found"
+  };
+
+  res.status(404).json(payload);
+}
